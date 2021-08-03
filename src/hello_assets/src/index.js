ed47cd0f71b0b2b514001d3e294adf8894355bbc
alyson <alyson.nextage@gmail.com>
2021-08-03 12:09:14 -0300

    Inclusão do bootstrap

--- src/hello_assets/src/index.js
+++ src/hello_assets/src/index.js
@@ -6,19 +6,19 @@ const hello = Actor.createActor(hello_idl, { agent, canisterId: hello_id });
 
 agent.fetchRootKey();
 
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const email = document.getElementById("email").value.toString();
+document.getElementById('form').addEventListener('submit', async (event) => {
+  event.preventDefault();
+  document.getElementById('greeting').innerText = '';
+  const email = document.getElementById('email').value.toString();
   const registerId = await hello.create(email);
-  console.log("RegisterId: " + registerId);
+  console.log('RegisterId: ' + registerId);
 
-  document.getElementById("greeting").innerText = `Thanks for the registration ` + email;
+  document.getElementById('greeting').innerText = 'Thanks for the registration ' + email;
 
   let op = await hello.read(registerId);
-  console.log("readEmail: " + op);
-  let entry = op && op.length > 0 ? opt_entry[0] : null;
-  if (entry === null || entry === undefined) {
-    entry = "";
+  let entry = op && op.length > 0 ? op[0] : null;
+  if (entry && entry.email) {
+    console.log('read: ' + entry.id + ' - ' + entry.email);
   }
-  document.getElementById("emails").value = entry;
-  
+  //document.getElementById('emails').value = entry;
 });
